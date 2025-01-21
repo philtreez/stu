a8eb1bdf51341c907307dd65ade5a0b0c828230c
philtreez <philipp.treezy@gmail.com>
2025-01-21 07:54:20 +0100

    j

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 async function setup() {
-    const patchExportURL = "export/patch.export.json";
+    const patchExportURL = "https://stu-philtreezs-projects.vercel.app/export/patch.export.json";
 
     // Create AudioContext
     const WAContext = window.AudioContext || window.webkitAudioContext;
